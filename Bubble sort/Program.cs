8125dfabc093bce3cae3ec4a89e11a36842a0508
Dan <66948931+Danicus2000000@users.noreply.github.com>
2024-11-14 13:03:30 +0000

    Migrated buble sort to NET 9

--- Bubble sort/Program.cs
+++ Bubble sort/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bubble_sort
+﻿namespace Bubble_sort
 {
     class Program
     {
-        public static void write_num(List<int> data)//a function that writes out all values in the given list
+        public static void WriteNum(List<int> data)//a function that writes out all values in the given list
         {
-            foreach(int value in data)
+            foreach (int value in data)
             {
                 Console.WriteLine(value);
             }
@@ -19,29 +13,29 @@ namespace Bubble_sort
         {
             // this region fetches ten random numbers between 1 and 100
             #region Get numbers
-            List<int> numbers = new List<int>();
-            Random RNG = new Random();
+            List<int> numbers = [];
+            Random RNG = new();
             for (int i = 0; i < 10; i++)
             {
                 numbers.Add(RNG.Next(0, 1000));
             }
             #endregion
             int temp;//holds value of number that gets overwritten
-            bool swap = false;//sees whether looping agin is nessasary
 
             Console.WriteLine("Before:");//shows numbers before
-            write_num(numbers);
+            WriteNum(numbers);
             Console.ReadLine();
+            bool swap;
             do
             {
                 swap = false;//sets swap to false so it will stop looping if a swap doesnt occur
-                for (int i = 0; i < numbers.Count-1; i++)//loops through all values in list
+                for (int i = 0; i < numbers.Count - 1; i++)//loops through all values in list
                 {
 
-                    if (numbers[i] > numbers[i+1])//checks if a value is larger than the next
+                    if (numbers[i] > numbers[i + 1])//checks if a value is larger than the next
                     {
                         temp = numbers[i];//moves it
-                        numbers[i] = numbers[i+1];
+                        numbers[i] = numbers[i + 1];
                         numbers[i + 1] = temp;
                         swap = true;//ensures looping continues
                     }
@@ -49,7 +43,7 @@ namespace Bubble_sort
             } while (swap == true);//condition to loop
             Console.WriteLine();//shows list after
             Console.WriteLine("After:");
-            write_num(numbers);
+            WriteNum(numbers);
             Console.ReadLine();
         }
     }
